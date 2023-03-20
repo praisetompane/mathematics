ce85f6a06a75828c6dc3fdd487935618c6fe8bc5
Praise Tompane <praisetompane@icloud.com>
2023-03-20 14:24:34 +0200

    citations and some problem sets. move notation to root folder as it is used across branches.

---
+++ 2_geometry/analytic_geometry/point_inversion.py
@@ -0,0 +1,13 @@
+"""
+    Context:
+        p = (pᵢ, pⱼ)
+
+"""
+
+def invert_point(pi, pj, qx, qy):
+    return [2*qx - pi, 2*qy - pj]
+
+"""
+References:
+    https://en.wikipedia.org/wiki/Point_reflection
+"""
