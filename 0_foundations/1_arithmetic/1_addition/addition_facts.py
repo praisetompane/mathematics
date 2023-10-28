3dddb535de403cee60b2c7f0aa15680e411afcc2
Praise Tompane <praisetompane@icloud.com>
2023-10-28 12:22:34 +0200

    Black formatted all Python code
    --date=0.day.ago

--- 0_foundations/1_arithmetic/1_addition/addition_facts.py
+++ 0_foundations/1_arithmetic/1_addition/addition_facts.py
@@ -1,22 +1,24 @@
-'''
+"""
     computes and prints basic addition facts upto 10
-'''
+"""
+
 
 def facts():
     total_numbers = 11
-    print('identity sum')
+    print("identity sum")
     for i in range(total_numbers):
-        print(f'{i} + {i} = {i + i}')
-    
-    print('adjacent sum')
+        print(f"{i} + {i} = {i + i}")
+
+    print("adjacent sum")
     for i in range(total_numbers - 1):
-        print(f'{i} + {i + 1} = {i + (i + 1)}')
-    
-    print('all possible combinations')
-    #can be halved due commutative law of addition 
+        print(f"{i} + {i + 1} = {i + (i + 1)}")
+
+    print("all possible combinations")
+    # can be halved due commutative law of addition
     for i in range(total_numbers):
         for j in range(i, total_numbers):
-            print(f'{i} + {j} = {i + j}')
-        
-if __name__ == '__main__':
+            print(f"{i} + {j} = {i + j}")
+
+
+if __name__ == "__main__":
     facts()
--- 1_algebra/9_matrices/matrix.py
+++ 1_algebra/9_matrices/matrix.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-def a0(): np.array([1, 3, 2], [4, 12, 8], [2, 6, 4])
+
+def a0():
+    np.array([1, 3, 2], [4, 12, 8], [2, 6, 4])
+
 
 print(a0)
